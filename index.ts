42c5cf201234664060ad70e232b4967d6753976a
lov3five <luongtranstaff@gmail.com>
2024-01-13 18:14:08 +0700

    Chew TS

--- index.ts
+++ index.ts
@@ -15,13 +15,13 @@ let lastName: string = "Luong";
 //console.log(`Hello ${firstName} ${lastName}`);
 
 interface IUser {
-    firstName: string;
-    lastName: string;
+  firstName: string;
+  lastName: string;
 }
 
 const user: IUser = {
-    firstName: "Tran",
-    lastName: "Naker",
+  firstName: "Tran",
+  lastName: "Naker",
 };
 //console.log(`Hello ${user.firstName} ${user.lastName}`);
 
@@ -30,26 +30,82 @@ const user: IUser = {
     protected: chỉ có thể truy cập trong class và các class kế thừa
     public: có thể truy cập ở bất cứ đâu
     constructor: hàm khởi tạo
+    static: thuộc tính tĩnh
 */
 class UserAccount {
-    firstName: string;
-    lastName: string;
+  firstName: string;
+  lastName: string;
 
-    constructor(firstName: string, lastName: string) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 
-    private getFullNamePrivate(): string {
-        return `${this.firstName} ${this.lastName}`;
-    }
+  private getFullNamePrivate(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
 
-    public getFullNamePublic(): string {
-        return this.getFullNamePrivate();
-    }
+  public getFullNamePublic(): string {
+    return this.getFullNamePrivate();
+  }
 }
 
 let user1 = new UserAccount("Tran", "Class");
-console.log(`Hello ${user1.getFullNamePublic()}`);
+//console.log(`Hello ${user1.getFullNamePublic()}`);
 
-// Generics là một cách để tạo ra các thành phần có thể làm việc với nhiều kiểu dữ liệu khác nhau mà không phải thêm nhiều code.
+class Caculator {
+  static sum(a: number, b: number): number {
+    return a + b;
+  }
+
+  static mul(a: number, b: number): number {
+    return a * b;
+  }
+
+  static div(a: number, b: number): number {
+    return a / b;
+  }
+
+  static sub(a: number, b: number): number {
+    return a - b;
+  }
+}
+
+let sum = Caculator.sum(1, 2);
+
+//console.log(sum);
+
+function wrapInArray(obj: string[] | string) {
+  if (typeof obj === "string") {
+    return [obj];
+  }
+  return obj;
+}
+
+//console.log(wrapInArray("hello"));
+
+/* 
+    Generics:
+    Generics cung cấp các biến cho các loại. Một ví dụ phổ biến là một mảng. 
+    Một mảng không có generics có thể chứa bất cứ thứ gì. 
+    Một mảng có Generics có thể mô tả các giá trị mà mảng chứa.
+*/
+
+// type StringArray = string[]
+type StringArray = Array<string>
+let stringArray: StringArray = ['a', 'b']
+console.log(stringArray);
+
+
+// type NumberArray = number[]
+type NumberArray = Array<number>
+let numberArray: NumberArray = [2,3,4,6]
+
+// type ObjectWithNameArray = {name: string}[]
+type ObjectWithNameArray = Array<{ name: string }>
+let objWithNameArray: ObjectWithNameArray = [
+  {name: 'Luong'},
+  {name: 'GitHub'}
+]
+
+
